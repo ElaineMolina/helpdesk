d61bab4020de6faa03239e78aa06a8fd19d31c3f
Elaine.Molina <elaine.molina@capgemini.com>
2023-05-31 09:21:55 -0300

    Finalizando componente tecnico-list

--- helpdesk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -11,35 +12,36 @@ import { Tecnico } from 'src/app/models/tecnico';
 
 export class TecnicoListComponent implements OnInit {
 
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Elaine Molina',
-      cpf: '123.456.789-10',
-      email: 'molina@mail.com',
-      senha: '1234',
-      perfis: ['0'],
-      dataCriacao: '30/05/2023'
-    }
-  ];
+  ELEMENT_DATA: Tecnico[] = [];
 
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private service: TecnicoService){}
 
   ngOnInit(): void {
-
+    this.findAll();
   }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
+
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta
+      this.dataSource = new MatTableDataSource<Tecnico>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }  
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+
+
