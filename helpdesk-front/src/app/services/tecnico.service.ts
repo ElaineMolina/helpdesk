e9abec5c21e9349d6416f0ca181bafa1f4aba4e0
Elaine.Molina <elaine.molina@capgemini.com>
2023-06-01 15:25:36 -0300

    Requisição de criação ao backend tecnico-create, add perfil checkbox

--- helpdesk-front/src/app/services/tecnico.service.ts
+++ helpdesk-front/src/app/services/tecnico.service.ts
@@ -1,9 +1,9 @@
+import { Tecnico } from './../models/tecnico';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { API_CONFIG } from '../config/api.config';
-import { Tecnico } from '../models/tecnico';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,9 @@ export class TecnicoService {
 
   findAll(): Observable<Tecnico[]> {
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
+  }
+
+  create(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
   }
 }
