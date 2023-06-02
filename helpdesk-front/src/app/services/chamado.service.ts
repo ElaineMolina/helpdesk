8f6408d47098aa9d52e0a9366343e7b8c960399f
Elaine.Molina <elaine.molina@capgemini.com>
2023-06-02 15:48:24 -0300

    Serviço para criar chamado implementado

--- helpdesk-front/src/app/services/chamado.service.ts
+++ helpdesk-front/src/app/services/chamado.service.ts
@@ -11,7 +11,11 @@ export class ChamadoService {
 
   constructor(private http: HttpClient) { }
 
-  findAll(): Observable<Chamado[]>{
+  findAll(): Observable<Chamado[]> {
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado);
   }
 }
